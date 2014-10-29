95a1b8e7c974d5e48e862fdd26f9d482c76432ef
Haroon Ahmed <hrahmed@gmail.com>
2014-10-29 18:04:40 -0400

    Initial Commit

---
+++ mathapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mathapp
+//
+//  Created by ahmha02 on 2014-10-29.
+//  Copyright (c) 2014 Haroon Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ mathappTests/mathappTests.swift
@@ -0,0 +1,36 @@
+//
+//  mathappTests.swift
+//  mathappTests
+//
+//  Created by ahmha02 on 2014-10-29.
+//  Copyright (c) 2014 Haroon Ahmed. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class mathappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
